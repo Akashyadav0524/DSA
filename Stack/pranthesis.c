e92132d0ce5c5b89811822077af8791f347121e5
Akash <akashy0524@gmail.com>
2022-05-09 22:13:17 +0530

    08.05.22

---
+++ Stack/pranthesis.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *top = NULL;
+
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int isEmpty(struct Node *top)
+{
+    if (top == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct Node *top)
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    if (p == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+
+int main()
+{
+    top = push(top, 78);
+    top = push(top, 7);
+    top = push(top, 8);
+
+    // linkedListTraversal(top);
+
+    
+   
+    linkedListTraversal(top);
+    return 0;
+}
